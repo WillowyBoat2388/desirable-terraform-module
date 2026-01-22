d7c256fa2554767ead5c131b25483012e4ed61a7
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-01-22 07:58:01 +0000

    fix: Issue with storage location solved

--- production/providers.tf
+++ production/providers.tf
@@ -34,9 +34,19 @@ provider "azurerm" {
 }
 provider "azapi" {}
 
+data "terraform_remote_state" "foo" {
+  backend = "azurerm"
+  config = {
+
+    storage_account_name = "dagsterinarian27"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
 provider "databricks" {
-  host                        = module.data-workflow.databricks_workspace_url != "" ? module.data-workflow.databricks_workspace_url : 0
-  azure_workspace_resource_id = module.data-workflow.databricks_workspace_resource_id != "" ? module.data-workflow.databricks_workspace_resource_id : 0
+  host                        = module.data-workflow.databricks_workspace_url != "" ? module.data-workflow.databricks_workspace_url : data.teraform_remote_state.foo.outputs.databricks_workspace_url
+  azure_workspace_resource_id = module.data-workflow.databricks_workspace_resource_id != "" ? module.data-workflow.databricks_workspace_resource_id : data.terraform_remote_state.foo.outputs.databricks_workspace_resource_id
   # auth_type                   = "azure-cli"
   
 }
