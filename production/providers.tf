44d13127138696bc320debefc6e9a40df6ab168e
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-01-22 08:02:07 +0000

    fix: Issue with storage location solved

--- production/providers.tf
+++ production/providers.tf
@@ -45,7 +45,7 @@ data "terraform_remote_state" "foo" {
 }
 
 provider "databricks" {
-  host                        = module.data-workflow.databricks_workspace_url != "" ? module.data-workflow.databricks_workspace_url : data.teraform_remote_state.foo.outputs.databricks_workspace_url
+  host                        = module.data-workflow.databricks_workspace_url != "" ? module.data-workflow.databricks_workspace_url : data.terraform_remote_state.foo.outputs.databricks_workspace_url
   azure_workspace_resource_id = module.data-workflow.databricks_workspace_resource_id != "" ? module.data-workflow.databricks_workspace_resource_id : data.terraform_remote_state.foo.outputs.databricks_workspace_resource_id
   # auth_type                   = "azure-cli"
   
