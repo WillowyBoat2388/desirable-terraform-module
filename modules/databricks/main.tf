95e4675351ec2359215428c97dad995270022064
architect-willow <fikayo.onidajo@bdatanet.tech>
2026-02-01 18:33:14 +0000

    fix: Validated terraform config. ready to run

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -50,10 +50,7 @@ data "databricks_node_type" "smallest" {
   depends_on = [data.databricks_spark_version.latest_lts]
 }
 
-data "databricks_catalog" "this" {
-  name       = var.workspace_name
-  depends_on = [data.databricks_spark_version.latest_lts]
-}
+data "databricks_catalogs" "this" {}
 
 data "databricks_current_metastore" "this" {
 
@@ -158,7 +155,7 @@ resource "databricks_external_location" "ong_data_stream" {
 
 resource "databricks_volume" "sensorstream" {
   name             = "ong_sensorstream"
-  catalog_name     = data.databricks_catalog.this.name
+  catalog_name     = data.databricks_catalogs.this.ids[0]
   schema_name      = "default"
   volume_type      = "EXTERNAL"
   storage_location = "${databricks_external_location.ong_data_stream.url}/analytics"
@@ -167,7 +164,7 @@ resource "databricks_volume" "sensorstream" {
 
 resource "databricks_volume" "checkPoints" {
   name         = "checkpoints"
-  catalog_name = data.databricks_catalog.this.name
+  catalog_name = data.databricks_catalogs.this.ids[0]
   schema_name  = "default"
   volume_type  = "MANAGED"
   comment      = "this volume is managed by terraform"
@@ -175,7 +172,7 @@ resource "databricks_volume" "checkPoints" {
 
 resource "databricks_schema" "bronze_layer" {
   name         = "landing"
-  catalog_name = data.databricks_catalog.this.name
+  catalog_name = data.databricks_catalogs.this.ids[0]
   properties = {
     kind = "various"
   }
@@ -184,7 +181,7 @@ resource "databricks_schema" "bronze_layer" {
 
 resource "databricks_schema" "bronze_layer2" {
   name         = "raw"
-  catalog_name = data.databricks_catalog.this.name
+  catalog_name = data.databricks_catalogs.this.ids[0]
   properties = {
     kind = "various"
   }
