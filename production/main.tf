b2382b57f8bf74c235a3f80f7a5ac498b13e8d55
architect-willow <fikayo.onidajo@bdatanet.tech>
2026-02-01 17:27:57 +0000

    fix: Validated terraform config. ready to run

--- production/main.tf
+++ production/main.tf
@@ -101,35 +101,35 @@ module "data-workflow" {
   depends_on = [module.global]
 }
 
-module "databricks" {
-  source = "../modules/databricks"
+# module "databricks" {
+#   source = "../modules/databricks"
 
-  # Input variables
-  environment                     = local.environment
-  location                        = local.region
-  prefix                          = local.prefix
-  owner                           = "architect"
-  team                            = var.team
-  rg_parent_id                    = azapi_resource.env.parent_id
-  controlid_name                  = local.controlid_name
-  environmentid_name              = local.environmentid_name
-  storage_account                 = module.data-workflow.storage_account_name
-  storage_container               = module.data-workflow.storage_container_name
-  service_connector               = module.data-workflow.databricks_service_connector
-  cluster_autotermination_minutes = 60
-  cluster_num_workers             = 1
-  cluster_data_security_mode      = "USER_ISOLATION"
-  key_vault                       = module.global.key_vault_name
-  github_pat                      = var.github_pat
-  github_username                 = var.github_username
-  github_email                    = var.github_email
-  jobsource_url                   = var.jobsource_url
-  slack_key                       = var.slack_key
-  workspace_name                  = module.data-workflow.databricks_workspace_name
-  rg_name                         = local.name
+#   # Input variables
+#   environment                     = local.environment
+#   location                        = local.region
+#   prefix                          = local.prefix
+#   owner                           = "architect"
+#   team                            = var.team
+#   rg_parent_id                    = azapi_resource.env.parent_id
+#   controlid_name                  = local.controlid_name
+#   environmentid_name              = local.environmentid_name
+#   storage_account                 = module.data-workflow.storage_account_name
+#   storage_container               = module.data-workflow.storage_container_name
+#   service_connector               = module.data-workflow.databricks_service_connector
+#   cluster_autotermination_minutes = 60
+#   cluster_num_workers             = 1
+#   cluster_data_security_mode      = "USER_ISOLATION"
+#   key_vault                       = module.global.key_vault_name
+#   github_pat                      = var.github_pat
+#   github_username                 = var.github_username
+#   github_email                    = var.github_email
+#   jobsource_url                   = var.jobsource_url
+#   slack_key                       = var.slack_key
+#   workspace_name                  = module.data-workflow.databricks_workspace_name
+#   rg_name                         = local.name
 
-  depends_on = [module.data-workflow]
+#   depends_on = [module.data-workflow]
 
 
-}
+# }
 
