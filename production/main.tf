76d8b5bea1c2a5e7faa07f34f95e3226bcb9c5a7
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-02-06 14:43:16 +0100

    trial

--- production/main.tf
+++ production/main.tf
@@ -25,17 +25,7 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = local.name
 }
 
-data "azurerm_key_vault_secret" "databricks_workspace_id" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = "databricks-workspace-resource-id"
-  depends_on   = [module.data-workflow]
-}
 
-data "azurerm_key_vault_secret" "databricks_workspace_url" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = "databricks-workspace-url"
-  depends_on   = [module.data-workflow]
-}
 
 locals {
   environment        = var.environment
@@ -46,8 +36,6 @@ locals {
   prefix             = "ong"
   controlid_name     = "floor_zero_admin"
   environmentid_name = "floor_zero_admin"
-  workspace_id       = data.azurerm_key_vault_secret.databricks_workspace_id.value
-  workspace_url      = data.azurerm_key_vault_secret.databricks_workspace_url.value
 
 }
 
@@ -122,35 +110,5 @@ module "data-workflow" {
   depends_on         = [module.global]
 }
 
-module "databricks" {
-  source = "../modules/databricks"
 
-  # Input variables
-  environment                     = local.environment
-  location                        = local.region
-  prefix                          = local.prefix
-  owner                           = "architect"
-  team                            = var.team
-  rg_parent_id                    = azapi_resource.env.parent_id
-  controlid_name                  = local.controlid_name
-  environmentid_name              = local.environmentid_name
-  storage_account                 = module.data-workflow.storage_account_name
-  storage_container               = module.data-workflow.storage_container_name
-  service_connector               = module.data-workflow.databricks_service_connector
-  cluster_autotermination_minutes = 60
-  cluster_num_workers             = 1
-  cluster_data_security_mode      = "USER_ISOLATION"
-  key_vault                       = module.global.key_vault_name
-  github_pat                      = var.github_pat
-  github_username                 = var.github_username
-  github_email                    = var.github_email
-  jobsource_url                   = var.jobsource_url
-  slack_key                       = var.slack_key
-  workspace_name                  = module.data-workflow.databricks_workspace_name
-  rg_name                         = local.name
-  parent                          = local.parent
-  depends_on                      = [module.data-workflow, data.azurerm_key_vault_secret.databricks_workspace_id]
-
-
-}
 
