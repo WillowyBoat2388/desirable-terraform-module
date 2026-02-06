e99ce8f1ef9d3bb17557005b73daefc56b95d383
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-02-06 11:18:57 +0000

    feat: Updated

--- production/main.tf
+++ production/main.tf
@@ -45,7 +45,7 @@ locals {
   domain             = "bdatanet.tech"
   prefix             = "ong"
   controlid_name     = "floor_zero_admin"
-  environmentid_name = "floor_zero_dmin"
+  environmentid_name = "floor_zero_admin"
   workspace_id       = can(data.azurerm_databricks_workspace.databricks_workspace.id) ? data.azurerm_databricks_workspace.databricks_workspace.id : data.azurerm_key_vault_secret.databricks_workspace_id.value
   workspace_url      = data.azurerm_databricks_workspace.databricks_workspace.workspace_url != null ? data.azurerm_databricks_workspace.databricks_workspace.workspace_url : data.azurerm_key_vault_secret.databricks_workspace_url.value
 
