b333f14595dfbfec134d050082c84995ce12f9ce
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-02-06 17:38:30 +0100

    fix: finallyyyy

--- production/main.tf
+++ production/main.tf
@@ -44,8 +44,8 @@ locals {
   parent             = "floor_zero"
   domain             = "bdatanet.tech"
   prefix             = "ong"
-  controlid_name     = "floor_zero_admin"
-  environmentid_name = "floor_zero_admin"
+  controlid_name     = "love_admin"
+  environmentid_name = "love_admin"
   workspace_id       = can(data.azurerm_databricks_workspace.databricks_workspace.id) ? data.azurerm_databricks_workspace.databricks_workspace.id : data.azurerm_key_vault_secret.databricks_workspace_id.value
   workspace_url      = data.azurerm_databricks_workspace.databricks_workspace.workspace_url != null ? data.azurerm_databricks_workspace.databricks_workspace.workspace_url : data.azurerm_key_vault_secret.databricks_workspace_url.value
 
