5776824fb6ac23ad5af9c82fa02de9e772f996fb
Onidajo Fikayo <fikayo.onidajo@bdatanet.tech>
2026-02-06 13:17:22 +0100

    temp-fix: saving updates

--- production/main.tf
+++ production/main.tf
@@ -46,8 +46,8 @@ locals {
   prefix             = "ong"
   controlid_name     = "floor_zero_admin"
   environmentid_name = "floor_zero_admin"
-  workspace_id       = can(data.azurerm_databricks_workspace.databricks_workspace.id) ? data.azurerm_databricks_workspace.databricks_workspace.id : data.azurerm_key_vault_secret.databricks_workspace_id.value
-  workspace_url      = data.azurerm_databricks_workspace.databricks_workspace.workspace_url != null ? data.azurerm_databricks_workspace.databricks_workspace.workspace_url : data.azurerm_key_vault_secret.databricks_workspace_url.value
+  workspace_id       = data.azurerm_key_vault_secret.databricks_workspace_id.value
+  workspace_url      = data.azurerm_key_vault_secret.databricks_workspace_url.value
 
 }
 
